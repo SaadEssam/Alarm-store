ddaf3dbd56bfce64d5b14f11c75ee85d9f7472ae
SaadEssam <SaadEssam059@gmail.com>
2023-05-16 15:16:41 +0300

    Build a 404 error page

---
+++ pages/404.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+const Error = () => {
+  return (
+    <div className="error404">
+      <Head>
+        <title>Page Not Found - Alarm Store</title>
+      </Head>
+      <h1 className="">404</h1>
+      <h3 className="">Sorry, but this page does not exist.</h3>
+      <Link href="/" className="Go-Home">
+        Homepage
+      </Link>
+    </div>
+  );
+};
+
+export default Error;
